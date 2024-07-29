775ce85350ee7c24a84bb71a53fef8c0da09c4be
diogenesOliver <diogenes.riboliveira@gmail.com>
2024-07-29 18:29:55 -0300

    wip: crating a new alarm from SQS service

--- terraform/cw.tf
+++ terraform/cw.tf
@@ -1,12 +1,28 @@
-resource "aws_cloudwatch_metric_alarm" "queue-metric" {
-  alarm_name          = "SQSQueueMessagesSent"
+resource "aws_cloudwatch_metric_alarm" "unprocessed-messages" {
+  alarm_name          = "SQSMessagesVisibles"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = 2
+  metric_name         = "ApproximateNumberOfMessagesVisible"
+  namespace           = "AWS/SQS"
+  period              = 300
+  statistic           = "Average"
+  threshold           = 50
+  alarm_description   = "This metric monitors number of messages visible"
+
+  dimensions = {
+    QueueName = aws_sqs_queue.sqs-api-test.name
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "messages-sent" {
+  alarm_name          = "NumberOfMessagesSent"
   comparison_operator = "GreaterThanOrEqualToThreshold"
   evaluation_periods  = 2
   metric_name         = "NumberOfMessagesSent"
   namespace           = "AWS/SQS"
   period              = 300
   statistic           = "Average"
-  threshold           = 30
+  threshold           = 0
   alarm_description   = "This metric monitors number of messages sent"
 
   dimensions = {
--- terraform/sqs.tf
+++ terraform/sqs.tf
@@ -1,6 +1,6 @@
 resource "aws_sqs_queue" "sqs-api-test" {
   name                       = "sqs-api-test"
-  delay_seconds              = 30
+  delay_seconds              = 15
   max_message_size           = 2048
   message_retention_seconds  = 600
   receive_wait_time_seconds  = 10
